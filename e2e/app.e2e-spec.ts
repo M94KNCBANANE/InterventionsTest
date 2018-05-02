e1e447f52d043584b74c2b1e5345dc6a4e67e854
Frederik sylvain <frederik_coolss@hotmail.com>
2018-05-02 11:22:08 -0400

    Fin TP18

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,39 @@
 import { AppPage } from './app.po';
 
-describe('interventions-test App', () => {
+describe('stocks App', () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('doit afficher le titre du formulaire Déclarer un problème', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to Inter!');
+    expect(page.getParagraphText()).toEqual('Déclarer un problème');
   });
+
+  it('doit activer le bouton Sauvegarder avec champs valides scénario nominal', () => {
+    page.setChampsValidesScenarioNominal();                    
+    expect(page.boutonSubmit().isEnabled()).toBe(true);
+  });   
+
+  it('doit activer le bouton Sauvegarder avec champs valides scénario alternatif Par message TEXTE', () => {
+    page.setChampsValidesScenarioAlternatifParMessageTexte();                    
+    expect(page.boutonSubmit().isEnabled()).toBe(true);
+  });
+
+  it('doit activer le bouton Sauvegarder avec champs valides scénario alternatif par courriel', () => {
+    page.setChampsValidesScenarioAlternatifParCourriel();                    
+    expect(page.boutonSubmit().isEnabled()).toBe(true);
+  });
+
+  it('zone Description du probleme a une bordure ROUGE si nombre de caractères insuffisant', () => {
+    page.setZoneDescriptionProblemeCaracteresInsuffisant();  
+    expect(page.obtenirClasseZoneNomProduit()).toContain('is-invalid');
+  });  
+
+  it('zone Description du probleme a une bordure VERTE si nombre de caractères Suffisant', () => {
+    page.setZoneDescriptionProblemeCaracteresSuffisant();  
+    expect(page.obtenirClasseZoneNomProduit()).toContain('is-valid');
+  });  
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,11 +1,62 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, ElementFinder } from 'protractor';
 
 export class AppPage {
   navigateTo() {
-    return browser.get('/');
+    return browser.get('/probleme');
   }
 
   getParagraphText() {
-    return element(by.css('Inter-root h1')).getText();
+    return element(by.css('Inter-root h5')).getText();
   }
+
+  setChampsValidesScenarioNominal() : void {
+    element(by.id('PrenomId')).sendKeys('Pierre-Paul');
+    element(by.id('NomId')).sendKeys('Pierre-Paul');
+    // Sélectionner le premier élément dans la zone de liste déroulante
+    element(by.id('noProblemeId')).all(by.tagName('option')).get(2).click();      
+    // Cliquer sur la première option du bouton radio
+    element.all(by.id('notificationId')).get(0).click();      
+    element(by.id('descriptionProblemeId')).sendKeys('Problème entre la chaise et le clavier...');
+  }
+
+   setChampsValidesScenarioAlternatifParMessageTexte() : void {
+    element(by.id('PrenomId')).sendKeys('tonprenom');
+    element(by.id('NomId')).sendKeys('tonnom');
+    // Sélectionner le X élément dans la zone de liste déroulante
+    element(by.id('noProblemeId')).all(by.tagName('option')).get(2).click();
+    // Cliquer sur le bouton radio voulu
+    element.all(by.id('notificationId')).get(2).click();
+    element(by.id('TelephoneId')).sendKeys('5141231234');
+    element(by.id('descriptionProblemeId')).sendKeys('Problème entre la chaise et le clavier...');
+   }
+   setChampsValidesScenarioAlternatifParCourriel() : void {
+    element(by.id('PrenomId')).sendKeys('tonprenom');
+    element(by.id('NomId')).sendKeys('tonnom');
+    // Sélectionner le X élément dans la zone de liste déroulante
+    element(by.id('noProblemeId')).all(by.tagName('option')).get(2).click();
+    // Cliquer sur le bouton radio voulu
+    element.all(by.id('notificationId')).get(1).click();
+    element(by.id('EmailId')).sendKeys('aa@bbb.com');
+    element(by.id('EmailValideId')).sendKeys('aa@bbb.com');
+    element(by.id('descriptionProblemeId')).sendKeys('Problème entre la chaise et le clavier...');
+   } 
+
+  boutonSubmit() : ElementFinder { 
+    return element(by.buttonText('Sauvegarder'));
+  }   
+
+  setZoneDescriptionProblemeCaracteresInsuffisant() : void {
+    element(by.id('descriptionProblemeId')).clear();
+    element(by.id('descriptionProblemeId')).sendKeys('XX');
+  }
+
+  setZoneDescriptionProblemeCaracteresSuffisant() : void {
+    element(by.id('descriptionProblemeId')).clear();
+    element(by.id('descriptionProblemeId')).sendKeys('XXXXX');
+  }
+
+
+  obtenirClasseZoneNomProduit()   { 
+    return element(by.id('descriptionProblemeId')).getAttribute("class");
+  }   
 }
